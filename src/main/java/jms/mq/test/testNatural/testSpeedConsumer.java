15686df2937ea5a1af6b114d97098cc33d0d15fa
xu <twtiyb@gmail.com>
2015-08-28 14:28:29 +0800

    twtiyb
    amq 测速

---
+++ src/main/java/jms/mq/test/testNatural/testSpeedConsumer.java
@@ -0,0 +1,39 @@
+package jms.mq.test.testNatural;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+import jms.mq.util.BrokeUtil;
+import jms.mq.util.Consumer;
+
+public class testSpeedConsumer {
+
+	public static void main(String[] args) throws JMSException {
+		Consumer consumer = BrokeUtil.getConsummer(null);
+		consumer.setTopicListener("xuchun.testTopicSpeed",
+				new MessageListener() {
+					public void onMessage(Message message) {
+						try {
+							System.out.println(((TextMessage) message)
+									.getText());
+						} catch (JMSException e) {
+							e.printStackTrace();
+						}
+					}
+				}, true);
+
+		consumer.setQueueListener("xuchun.testQueueSpeed",
+				new MessageListener() {
+					public void onMessage(Message message) {
+						try {
+							System.out.println(((TextMessage) message)
+									.getText());
+						} catch (JMSException e) {
+							e.printStackTrace();
+						}
+					}
+				});
+	}
+}
